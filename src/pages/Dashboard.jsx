629776a362593f95ca20467139c8d8664d488155
mlol22 <86674912+mlol22@users.noreply.github.com>
2025-05-15 16:36:53 +0300

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,1 +1,22 @@
+import React from "react";
+import OverviewChart from "../components/OverviewChart";
+import DailyActivityChart from "../components/DailyActivityChart";
+import Alerts from "../components/Alerts";
+import TransactionSummary from "../components/TransactionSummary";
+import ClientFeedback from "../components/ClientFeedback";
+
+function Dashboard() {
+  return (
+    <div style={{ fontFamily: "Arial", padding: "2rem" }}>
+      <h1>Fraud Detection Dashboard</h1>
+      <OverviewChart />
+      <DailyActivityChart />
+      <Alerts />
+      <TransactionSummary />
+      <ClientFeedback />
+    </div>
+  );
+}
+
+export default Dashboard;
 
