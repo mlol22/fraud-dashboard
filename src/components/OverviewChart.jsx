a43f42c25c5a42175f8b90a0d507cb6db14064b0
mlol22 <86674912+mlol22@users.noreply.github.com>
2025-05-15 16:37:41 +0300

    Update OverviewChart.jsx

--- src/components/OverviewChart.jsx
+++ src/components/OverviewChart.jsx
@@ -1,1 +1,28 @@
+import React from "react";
+import { BarChart, Bar, XAxis, YAxis, Tooltip, CartesianGrid } from "recharts";
+
+const data = [
+  { day: "Mon", volume: 12000 },
+  { day: "Tue", volume: 18000 },
+  { day: "Wed", volume: 10000 },
+  { day: "Thu", volume: 24000 },
+  { day: "Fri", volume: 16000 },
+];
+
+function OverviewChart() {
+  return (
+    <div>
+      <h2>Transaction Volume Overview</h2>
+      <BarChart width={600} height={300} data={data}>
+        <XAxis dataKey="day" />
+        <YAxis />
+        <Tooltip />
+        <CartesianGrid stroke="#ccc" />
+        <Bar dataKey="volume" fill="#8884d8" />
+      </BarChart>
+    </div>
+  );
+}
+
+export default OverviewChart;
 
