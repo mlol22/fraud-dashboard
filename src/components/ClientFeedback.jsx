115b66b214f4c081bf8eeeeff97d3e4e19fb6eda
mlol22 <86674912+mlol22@users.noreply.github.com>
2025-05-15 16:41:53 +0300

    Create ClientFeedback.jsx

---
+++ src/components/ClientFeedback.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function ClientFeedback() {
+  return (
+    <div>
+      <h2>Client Feedback</h2>
+      <blockquote>
+        "The new fraud alert system helped us prevent a major breach."
+        <footer>- Security Analyst, BankX</footer>
+      </blockquote>
+    </div>
+  );
+}
+
+export default ClientFeedback;
